a0912ddd2ed6c4d7f53cbd30a2024415998f5f07
Cam <camarengaineslinuxsandbox@gmail.com>
2021-07-06 08:45:48 -0600

    First java program

---
+++ Gaines_Camaren_FirstJavaProgram.java
@@ -0,0 +1,8 @@
+package Gaines_Camaren_FirstJavaProgram;
+public class Gaines_Camaren_FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Camaren Gaines");
+        System.out.println("I am 22 years old");
+        System.out.println("My hometown is Kalispell");
+    }
+}
